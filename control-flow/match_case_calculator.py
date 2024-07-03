947bf961a6cd47e7bc1ab1ec9e31358c61edc567
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-03 21:33:33 +0200

    Updating match_case_calculation.py file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,22 @@
 #Variables that prompt users to enter three pieces of information.
 num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
-operation_type = input("+, -, *, /: ")
+operand = input("+, -, *, /: ")
 
-#
-match operation_type: 
+#Match, case allows the user to do a calculation.
+match operand: 
     case "+":
         result = num1 + num2
-        print(f"The result is [{result}].")
+        print(f"The result is {result}.")
     case "-":
         result = num1 - num2
-        print(f"The result is [{result}].")
+        print(f"The result is {result}.")
     case "*":
         result = num1 * num2
-        print(f"The result is [{result}].")
+        print(f"The result is {result}.")
     case "/":
         result = num1 / num2
-        print(f"The result is [{result}].")
+        print(f"The result is {result}.")
     case "/" if num2 == 0:
         result = num1 / num2
-        print(f"Cannot divide by zero.")
+        print("Cannot divide by zero.")
