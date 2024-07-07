955ef6570a24c93862e2c4f00bf3d841105991df
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-07 15:55:17 +0200

    Updating weather_advice.py file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,7 +3,7 @@ weather = str(input("What's the weather like today? (sunny/rainy/cold): "))
 
 #Provides clothing recommendations based on user input.
 if weather == 'sunny':
-    print("Wear a t-shirt and sungglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == 'rainy': 
       print("Don't forget your umbrella and a raincoat.")
 elif weather == 'cold':
