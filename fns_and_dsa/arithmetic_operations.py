e675f9c6b28ada01c256bdcaa72f5dc5052a4551
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-11 02:48:32 +0200

    Updating arithmatic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,16 +2,12 @@ def perform_operation(num1, num2, operation):
    match operation: 
     case "add":
         result = num1 + num2
-        print(f"The result is {result}.")
     case "subtract":
         result = num1 - num2
-        print(f"The result is {result}.")
     case "multiply":
         result = num1 * num2
-        print(f"The result is {result}.")
     case "divide":
         result = num1 / num2
-        print(f"The result is {result}.")
     case "divide" if num2 == 0:
         result = num1 / num2
-        return result
+        return result 
