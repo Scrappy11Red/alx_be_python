aaa168c0a71a28b02c597fbcac528e0fa03026d9
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-11 02:45:10 +0200

    arthmatic_operation.py updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -14,4 +14,4 @@ def perform_operation(num1, num2, operation):
         print(f"The result is {result}.")
     case "divide" if num2 == 0:
         result = num1 / num2
-        print("Cannot divide by zero.")
+        return result
