d267155e75342c946a43b25d38569c4904144d7e
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-11 02:52:16 +0200

    Updating arithmatic_operation.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,12 @@
-def perform_operation(num1, num2, operation):
-   match operation: 
-    case "add":
+def perform_operation(num1, num2, operation): 
+    if operation == "add":
         result = num1 + num2
-    case "subtract":
+    elif operation == "subtract":
         result = num1 - num2
-    case "multiply":
+    elif operation == "multiply":
         result = num1 * num2
-    case "divide":
+    elif operation == "divide":
         result = num1 / num2
-    case "divide" if num2 == 0:
-        result = num1 / num2
-        return result 
+        if num2 == 0:
+           result = num1 / num2
+           return result 
