0820977627860bbefb4d97b0eb3435040a5da9b0
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-20 22:59:12 +0200

    adding programming_paradigm directory, updating explore_datetime.py file and adding bank_account.py and main-0.py files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, account_balance, initial_balance = 0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+
+        def deposit(amount): 
+              account_balance + amount
+        
+        def withdraw(amount): 
+            if account_balance >= amount:
+              account_balance =- amount
+              return True
+            else: 
+                return False
+        
+        def display_balance(): 
+            return f"Acoount balance is {account_balance}"
+        
